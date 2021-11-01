9d79a33ab721fe749d733267cf283d58bac4a4e5
bopre <bopre>
2021-11-01 12:48:09 +0300

    Init calcEngine module

---
+++ calcEngine/src/test/kotlin/org/bopre/samples/calcEngine/CalcEngineApplicationTests.kt
@@ -0,0 +1,13 @@
+package org.bopre.samples.calcEngine
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CalcEngineApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
