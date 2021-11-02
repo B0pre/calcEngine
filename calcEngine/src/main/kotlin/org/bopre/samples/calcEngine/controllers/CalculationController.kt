ced918052941a0669f9b375087e2feff2c066489
bopre <bopre>
2021-11-02 15:39:48 +0300

    Removed deprecated rest methods

--- calcEngine/src/main/kotlin/org/bopre/samples/calcEngine/controllers/CalculationController.kt
+++ calcEngine/src/main/kotlin/org/bopre/samples/calcEngine/controllers/CalculationController.kt
@@ -12,30 +12,6 @@ import org.springframework.web.bind.annotation.RestController
 @RestController
 class CalculationController(@Autowired val calculationService: CalculationService) : CalculateExpressionsApi {
 
-    @GetMapping("/sum")
-    @Deprecated(message = "to be removed", replaceWith = ReplaceWith("nothing"))
-    fun sumOperation(@RequestParam("a") a: Double, @RequestParam("b") b: Double): OutputResult {
-        return calculationService.sumOperation(a, b);
-    }
-
-    @GetMapping("/diff")
-    @Deprecated(message = "to be removed", replaceWith = ReplaceWith("nothing"))
-    fun diffOperation(@RequestParam("a") a: Double, @RequestParam("b") b: Double): OutputResult {
-        return calculationService.diffOperation(a, b);
-    }
-
-    @GetMapping("/div")
-    @Deprecated(message = "to be removed", replaceWith = ReplaceWith("nothing"))
-    fun divOperation(@RequestParam("a") a: Double, @RequestParam("b") b: Double): OutputResult {
-        return calculationService.divOperation(a, b);
-    }
-
-    @GetMapping("/mul")
-    @Deprecated(message = "to be removed", replaceWith = ReplaceWith("nothing"))
-    fun mulOperation(@RequestParam("a") a: Double, @RequestParam("b") b: Double): OutputResult {
-        return calculationService.mulOperation(a, b);
-    }
-
     override fun expressionCalc(inputExpression: InputExpression?): ResponseEntity<OutputResult> {
         return ResponseEntity.ok(calculationService.expression(inputExpression!!))
     }
--- calcEngine/src/test/kotlin/org/bopre/samples/calcEngine/controllers/CalculationControllerTest.kt
+++ calcEngine/src/test/kotlin/org/bopre/samples/calcEngine/controllers/CalculationControllerTest.kt
@@ -10,7 +10,6 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
 import org.springframework.boot.test.mock.mockito.MockBean
 import org.springframework.http.MediaType
 import org.springframework.test.web.servlet.MockMvc
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers.content
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
@@ -20,66 +19,6 @@ class CalculationControllerTest(@Autowired val mockMvc: MockMvc) {
 
     @MockBean
     lateinit var calculationService: CalculationService
-
-    @Test
-    fun `sum rest request`() {
-        val expectedJson = "{\n" +
-                "\"result\": 15\n" +
-                "}\n";
-
-        Mockito.`when`(calculationService.sumOperation(5.0, 10.0))
-            .thenReturn(OutputResult(15.0))
-
-        mockMvc.perform(get("/sum?a=5&b=10"))
-            .andExpect(status().isOk)
-            .andExpect(content().contentType(MediaType.APPLICATION_JSON))
-            .andExpect(content().json(expectedJson))
-    }
-
-    @Test
-    fun `diff rest request`() {
-        val expectedJson = "{\n" +
-                "\"result\": -5\n" +
-                "}\n";
-
-        Mockito.`when`(calculationService.diffOperation(5.0, 10.0))
-            .thenReturn(OutputResult(-5.0))
-
-        mockMvc.perform(get("/diff?a=5&b=10"))
-            .andExpect(status().isOk)
-            .andExpect(content().contentType(MediaType.APPLICATION_JSON))
-            .andExpect(content().json(expectedJson))
-    }
-
-    @Test
-    fun `div rest request`() {
-        val expectedJson = "{\n" +
-                "\"result\": 0.5\n" +
-                "}\n";
-
-        Mockito.`when`(calculationService.divOperation(5.0, 10.0))
-            .thenReturn(OutputResult(0.5))
-
-        mockMvc.perform(get("/div?a=5&b=10"))
-            .andExpect(status().isOk)
-            .andExpect(content().contentType(MediaType.APPLICATION_JSON))
-            .andExpect(content().json(expectedJson))
-    }
-
-    @Test
-    fun `mul rest request`() {
-        val expectedJson = "{\n" +
-                "\"result\": 50\n" +
-                "}\n";
-
-        Mockito.`when`(calculationService.mulOperation(5.0, 10.0))
-            .thenReturn(OutputResult(50.0))
-
-        mockMvc.perform(get("/mul?a=5&b=10"))
-            .andExpect(status().isOk)
-            .andExpect(content().contentType(MediaType.APPLICATION_JSON))
-            .andExpect(content().json(expectedJson))
-    }
 
     @Test
     fun `expression rest request`() {
